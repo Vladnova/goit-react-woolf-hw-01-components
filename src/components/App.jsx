d2691f7a0194016467811a06d3ed0128e714176d
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-02-08 20:35:28 +0200

    finished second home work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import Profile from './Profile/Profile';
-import user from '../components/Profile/user.json';
+import user from '../components/data/user.json';
+import data from '../components/data/data.json';
+import Statistics from './Statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data}/>
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import styles from '../Statistics/Statistics.module.css';
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className={styles.statistics}>
+      {title&&<h2 className={styles.title}>{title}</h2>}
+
+      <ul className={styles.list}>
+        {stats.map(({id, label, percentage})=>(
+          <li className={styles.item} style={{backgroundColor: getRandomColor()}} key={id}>
+          <span className={styles.label}>{label}</span>
+          <span className={styles.percentage}>{percentage}%</span>
+        </li>
+        ))}
+
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
